f6fb97d304e668a95973945b57c477d560abb2ce
PedroVazN <vaznascimento23@gmail.com>
2023-09-27 23:01:23 -0300

    Update

---
+++ 2º Semestre Mini Projetos/TestaBanco1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TestaBanco1 {
+    Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        banco1 SenaiBank = new banco1(0, null);
+        
+        SenaiBank.menu();
+
+    }
+}
---
+++ 2º Semestre Mini Projetos/TestaProduto.java
@@ -0,0 +1,29 @@
+public class TestaProduto {
+
+    public static void main(String[] args) {
+
+        produto p1 = new produto();
+        produto p2 = new produto();
+        produto p3 = new produto();
+
+        p1.setNome("DIAMANTES\n");
+        p1.setPreco(100);
+        p1.setQuantidadeEmEstoque(10);
+
+        p2.setNome("VBUCKS\n");
+        p2.setPreco(200);
+        p2.setQuantidadeEmEstoque(20);
+
+        p3.setNome("ROBUX\n");
+        p3.setPreco(300);
+        p3.setQuantidadeEmEstoque(30);
+
+        System.out.println(p1.getNome() + " \nPreço: " + p1.getPreco() + " \nEstoque: " + p1.getQuantidadeEmEstoque() + " \nTotal: " + p1.setCalcularTotal() + ".\n");
+        System.out.println(p2.getNome() + " \nPreço: " + p2.getPreco() + " \nEstoque: " + p2.getQuantidadeEmEstoque() + " \nTotal: " + p2.setCalcularTotal() + ".\n");
+        System.out.println(p3.getNome() + " \nPreço: " + p3.getPreco() + " \nEstoque: " + p3.getQuantidadeEmEstoque() + " \nTotal: " + p3.setCalcularTotal() + ".\n");
+
+        
+
+    }
+    
+}
